ec1d11d15b993f947bdf9e5f2e75712088abac01
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 18:33:16 +0330

    Model : Person for extending admin technician and customer created

---
+++ src/main/java/ir/hosseindn/model/Person.java
@@ -0,0 +1,40 @@
+package ir.hosseindn.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@ToString
+@SuperBuilder
+@MappedSuperclass
+public class Person extends BaseEntity<Long> {
+    @NotNull(message = "FirstName must not be null")
+    @Pattern(regexp = "^[A-Z](?=.{1,29}$)[A-Za-z]*(?:\\h+[A-Z][A-Za-z]*)*$")
+    String firstName;
+    @NotNull(message = "LastName must not be null")
+    @Pattern(regexp = "^[A-Z](?=.{1,29}$)[A-Za-z]*(?:\\h+[A-Z][A-Za-z]*)*$")
+    String lastName;
+    @NotNull
+    @Column(unique = true)
+    @Email(message = "Email must be valid")
+    String email;
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8}$")
+    String password;
+    LocalDate registeredDate;
+    @OneToOne(cascade = {CascadeType.PERSIST,CascadeType.MERGE,CascadeType.REMOVE})
+    Wallet wallet;
+}
