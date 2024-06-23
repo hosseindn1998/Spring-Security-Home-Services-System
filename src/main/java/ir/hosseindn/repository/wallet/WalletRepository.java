f7453cff87f585ee0a7308326b284cbc759fada0
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 20:02:10 +0330

    Repository : WalletRepository interface created

---
+++ src/main/java/ir/hosseindn/repository/wallet/WalletRepository.java
@@ -0,0 +1,9 @@
+package ir.hosseindn.repository.wallet;
+
+import ir.hosseindn.model.Wallet;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet,Long> {
+}
