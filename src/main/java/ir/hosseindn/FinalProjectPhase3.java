d28be0312001cf7f3f253ce27d1f59c2432b0f86
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 18:12:56 +0330

    In the name of god :first commit - dependencies added to pom.xlm

---
+++ src/main/java/ir/hosseindn/FinalProjectPhase3.java
@@ -0,0 +1,11 @@
+package ir.hosseindn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProjectPhase3 {
+    public static void main(String[] args) {
+        SpringApplication.run(FinalProjectPhase3.class,args);
+    }
+}
