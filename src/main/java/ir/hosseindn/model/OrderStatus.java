72e2c3a1c5f7cbdf4efd017d5ff6b1e692cd22d4
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 18:49:09 +0330

    Model : OrderStatus enum created

---
+++ src/main/java/ir/hosseindn/model/OrderStatus.java
@@ -0,0 +1,10 @@
+package ir.hosseindn.model;
+
+public enum OrderStatus {
+    WAIT_FOR_TECHNICIAN_OFFER,
+    WAIT_FOR_CHOOSE_TECHNICIAN,
+    WAIT_FOR_COME_TECHNICIAN,
+    STARTED,
+    DONE,
+    PAYED
+}
