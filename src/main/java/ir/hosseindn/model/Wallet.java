e3aaf972a0b6d12b8de2666c8bb5e0c6700fb722
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 18:31:56 +0330

    wallet for persons created : fields : amount

---
+++ src/main/java/ir/hosseindn/model/Wallet.java
@@ -0,0 +1,26 @@
+package ir.hosseindn.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@SuperBuilder
+@ToString(callSuper = true)
+@Table(name = "wallet")
+
+public class Wallet extends BaseEntity<Long> {
+    @NotNull
+    @Min(0)
+    Long amount;
+
+}
