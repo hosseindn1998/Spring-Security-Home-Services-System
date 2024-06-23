93b55324599d03ce566f89c9183eadc7c5ffef1e
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 19:11:20 +0330

    Model : sub service added

---
+++ src/main/java/ir/hosseindn/model/SubService.java
@@ -0,0 +1,39 @@
+package ir.hosseindn.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@SuperBuilder
+@ToString(callSuper = true)
+@Table(name = "subService")
+public class SubService extends BaseEntity<Long> {
+    @Column(unique = true)
+    String name;
+    @Min(0)
+    Long basePrice;
+    @NotNull
+    String description;
+    @ManyToOne
+    MainService mainService;
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REMOVE})
+    @ToString.Exclude
+    List<Technician> technicians;
+    @ToString.Exclude
+    @OneToMany(mappedBy = "subservice", cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REMOVE})
+    List<Order> orders;
+    @OneToMany(mappedBy = "subService",cascade = {CascadeType.REMOVE})
+    @ToString.Exclude
+    List<TechnicianSubService>technicianSubServices;
+}
