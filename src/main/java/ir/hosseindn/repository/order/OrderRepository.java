f6f35f1886d4eba0b86d9e52cad375459c5cc330
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 20:30:00 +0330

    Repository : OrderRepository interface created

---
+++ src/main/java/ir/hosseindn/repository/order/OrderRepository.java
@@ -0,0 +1,12 @@
+package ir.hosseindn.repository.order;
+
+import ir.hosseindn.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+
+
+}
+
