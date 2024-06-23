d2b743ed72d1f1c04b3a7ecb31fc6c8c68fdcf4f
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 19:33:41 +0330

    Repository : Admin repository : Create login method

--- src/main/java/ir/hosseindn/repository/admin/AdminRepository.java
+++ src/main/java/ir/hosseindn/repository/admin/AdminRepository.java
@@ -4,7 +4,9 @@ import ir.hosseindn.model.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
-
+    Optional<Admin> findByEmailAndPassword(String email, String password);
 }
