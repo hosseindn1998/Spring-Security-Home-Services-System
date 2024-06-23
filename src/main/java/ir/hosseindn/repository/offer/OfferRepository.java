9e6ca56f4f843027fbe03016fffccffdba90f28f
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 20:31:38 +0330

    Repository : OfferRepository interface created

---
+++ src/main/java/ir/hosseindn/repository/offer/OfferRepository.java
@@ -0,0 +1,10 @@
+package ir.hosseindn.repository.offer;
+
+import ir.hosseindn.model.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfferRepository extends JpaRepository<Offer,Long> {
+
+}
