931153d26dc40bd63c79e7fd10df4f5c7b54ec8c
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 19:16:14 +0330

    Model : Offer created

---
+++ src/main/java/ir/hosseindn/model/Offer.java
@@ -0,0 +1,36 @@
+package ir.hosseindn.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@SuperBuilder
+@ToString(callSuper = true)
+@Table(name = "offer")
+public class Offer extends BaseEntity<Long> {
+    @ManyToOne
+    @ToString.Exclude
+    Order odrer;
+    LocalDate dateOfOfferToStart;
+    @Min(0)
+    @NotNull
+    Long suggestPrice;
+    LocalDate dateOfOfferToDone;
+    @ManyToOne
+    Technician technician;
+    Boolean isAccepted;
+
+}
