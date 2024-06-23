e98395bfe9ad1646fa11fa3eccfec618f773450f
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 21:27:42 +0330

    Exception :Duplicate Information Exception custom created

---
+++ src/main/java/ir/hosseindn/exception/DuplicateInformationException.java
@@ -0,0 +1,7 @@
+package ir.hosseindn.exception;
+
+public class DuplicateInformationException extends RuntimeException{
+    public DuplicateInformationException(String message) {
+        super(message);
+    }
+}
