82327a9298e0dd3ad12da1b9e360595ebd9cf7ec
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 20:01:02 +0330

    Repository : Technician Repository : login method for technician created

--- src/main/java/ir/hosseindn/repository/technician/TechnicianRepository.java
+++ src/main/java/ir/hosseindn/repository/technician/TechnicianRepository.java
@@ -4,8 +4,11 @@ import ir.hosseindn.model.Technician;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface TechnicianRepository extends JpaRepository<Technician,Long> {
 
+    Optional<Technician> findByEmailAndPassword(String email, String password);
 
 }
