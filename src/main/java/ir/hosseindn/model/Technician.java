2382a17fbcbbabc0a573cd179c52b7fce5c7e9c7
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 19:02:24 +0330

    Model : Technician that extended Person

---
+++ src/main/java/ir/hosseindn/model/Technician.java
@@ -0,0 +1,48 @@
+package ir.hosseindn.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter()
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@SuperBuilder
+@ToString(callSuper = true)
+@Table(name = "Technician")
+public class Technician extends Person {
+    @Enumerated(EnumType.ORDINAL)
+    TechnicianStatus technicianStatus;
+    @NotNull
+    @Min(0)
+    @Max(5)
+    double rate;
+    @Lob
+    @ToString.Exclude
+    @Lazy
+    byte[] avatar;
+    @ManyToMany( mappedBy = "technicians", cascade = {CascadeType.MERGE, CascadeType.REMOVE})
+    @ToString.Exclude
+    List<SubService> subServices;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "technician", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @ToString.Exclude
+    List<Comment> comments;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "technician", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @ToString.Exclude
+    List<Offer> offers;
+    @OneToMany(fetch = FetchType.EAGER,mappedBy = "technician", cascade ={CascadeType.REMOVE})
+    @ToString.Exclude
+    List<TechnicianSubService>technicianSubServices;
+
+
+}
