39a4862ab4f6ece3f57d6bc00b06fa1c37d0c6f9
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 21:30:58 +0330

    Exception :Controller Advice > Global exception handler>Exception Handler method for duplicate information exception added

--- src/main/java/ir/hosseindn/exception/GlobalExceptionHandler.java
+++ src/main/java/ir/hosseindn/exception/GlobalExceptionHandler.java
@@ -1,11 +1,22 @@
 package ir.hosseindn.exception;
 
+import ir.hosseindn.dto.ExceptionDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-
+    @ExceptionHandler(DuplicateInformationException.class)
+    public ResponseEntity<ExceptionDto> duplicateInformationExceptionHandler(DuplicateInformationException e) {
+        log.warn(e.getMessage());
+        ExceptionDto exceptionDto = new ExceptionDto(e.getMessage(), LocalDateTime.now());
+        return new ResponseEntity<>(exceptionDto, HttpStatus.CONFLICT);
+    }
 
 }
