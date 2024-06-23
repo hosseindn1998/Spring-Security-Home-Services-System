2ac26182b92c45dbb456fd70804fab717bab26c8
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-23 19:16:02 +0330

    Model : Order created

---
+++ src/main/java/ir/hosseindn/model/Order.java
@@ -0,0 +1,49 @@
+package ir.hosseindn.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@SuperBuilder
+@ToString(callSuper = true)
+@Table(name = "odrer")
+public class Order extends BaseEntity<Long> {
+    @ManyToOne
+    SubService subservice;
+    @ManyToOne
+    Customer customer;
+    @Min(0L)
+    Long suggestedPrice;
+    @NotNull
+    Boolean isPaid;
+    @NotNull
+    @Pattern(regexp = "^[A-Z](?=.{1,29}$)[A-Za-z]*(?:\\h+[A-Z][A-Za-z]*)*$")
+    String description;
+    @NotNull
+    LocalDate dateForDo;
+    @NotNull
+    @Pattern(regexp = "^[A-Z](?=.{1,29}$)[A-Za-z]*(?:\\h+[A-Z][A-Za-z]*)*$")
+    String address;
+    @Enumerated
+    OrderStatus orderStatus;
+    @OneToMany(mappedBy = "odrer", cascade = {CascadeType.REMOVE})
+    @ToString.Exclude
+    List<Offer> offers;
+    @OneToOne
+    Offer choosedOffer;
+    @OneToOne
+    Comment comment;
+}
