3ed84e695da63f8088ffe409d0b58ba7939da946
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 03:26:54 +0330

    Service : CustomerService > create login method

--- src/main/java/ir/hosseindn/service/customer/CustomerService.java
+++ src/main/java/ir/hosseindn/service/customer/CustomerService.java
@@ -1,8 +1,8 @@
 package ir.hosseindn.service.customer;
 
-import ir.hosseindn.dto.customer.CustomerChangePasswordResponse;
 import ir.hosseindn.exception.DuplicateInformationException;
 import ir.hosseindn.exception.NotFoundException;
+import ir.hosseindn.exception.NotValidInformation;
 import ir.hosseindn.model.Customer;
 import ir.hosseindn.repository.customer.CustomerRepository;
 import lombok.RequiredArgsConstructor;
@@ -12,16 +12,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
-    public Customer register(Customer customer){
-        if(customerRepository.findByEmailOrNationalCode(customer.getEmail(),customer.getNationalCode()).isPresent())
+
+    public Customer register(Customer customer) {
+        if (customerRepository.findByEmailOrNationalCode(customer.getEmail(), customer.getNationalCode()).isPresent())
             throw new DuplicateInformationException("A Customer with this Email/National Code exist.");
         return customerRepository.save(customer);
     }
-    public Customer changePassword(String email,String newPassword){
-       Customer customer= customerRepository.findByEmail(email).orElseThrow(
-                ()->new NotFoundException("Customer with email :"+ email+ " Not found.")
+
+    public Customer changePassword(String email, String newPassword) {
+        Customer customer = customerRepository.findByEmail(email).orElseThrow(
+                () -> new NotFoundException("Customer with email :" + email + " Not found.")
         );
-       customerRepository.updatePassword(email, newPassword);
-       return customer;
+        customerRepository.updatePassword(email, newPassword);
+        return customer;
+    }
+
+    public Customer login(String email, String Password) {
+        return customerRepository.findByEmailAndPassword(email, Password).orElseThrow(
+                () -> new NotValidInformation("Email or Password is Incorrect")
+        );
     }
 }
