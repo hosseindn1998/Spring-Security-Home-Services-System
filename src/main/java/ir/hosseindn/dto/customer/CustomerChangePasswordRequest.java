b52e27bd180186d05b546a4eead2fb436db9ea17
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-24 22:36:24 +0330

    Dto : CustomerChangePasswordRequest created

---
+++ src/main/java/ir/hosseindn/dto/customer/CustomerChangePasswordRequest.java
@@ -0,0 +1,13 @@
+package ir.hosseindn.dto.customer;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
+public record CustomerChangePasswordRequest(
+        @Email(message = "Email must be valid")
+        String email,
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8}$",message ="Password Not valid ")
+        String password,
+        String confirmPassword
+) {
+}
--- src/main/java/ir/hosseindn/exception/GlobalExceptionHandler.java
+++ src/main/java/ir/hosseindn/exception/GlobalExceptionHandler.java
@@ -1,9 +1,11 @@
 package ir.hosseindn.exception;
 
 import ir.hosseindn.dto.ExceptionDto;
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
