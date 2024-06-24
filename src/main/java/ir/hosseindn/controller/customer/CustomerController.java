0617e2528ad3e7d612d4f59b7497bb5372b62236
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-24 22:42:03 +0330

    Controller : CustomerController > create change password method

--- src/main/java/ir/hosseindn/controller/customer/CustomerController.java
+++ src/main/java/ir/hosseindn/controller/customer/CustomerController.java
@@ -1,5 +1,7 @@
 package ir.hosseindn.controller.customer;
 
+import ir.hosseindn.dto.customer.CustomerChangePasswordRequest;
+import ir.hosseindn.dto.customer.CustomerChangePasswordResponse;
 import ir.hosseindn.dto.customer.CustomerSaveRequest;
 import ir.hosseindn.dto.customer.CustomerSaveResponse;
 import ir.hosseindn.exception.NotValidInformation;
@@ -12,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,5 +33,13 @@ public class CustomerController {
         Customer mappedCustomer = CustomerMapper.INSTANCE.customerSaveRequestToModel(request);
         Customer savedCustomer = customerService.register(mappedCustomer);
         return new ResponseEntity<>(CustomerMapper.INSTANCE.modelToUserSaveResponse(savedCustomer), HttpStatus.CREATED);
+    }
+    @PatchMapping("/customer-changePassword")
+    public ResponseEntity<CustomerChangePasswordResponse> changePassword(@Validated @RequestBody CustomerChangePasswordRequest request) {
+        if(!request.password().equals(request.confirmPassword()))
+            throw new NotValidInformation("new password must be match by confirm");
+        Customer mappedCustomer = CustomerMapper.INSTANCE.INSTANCE.customerChangePasswordRequestToModel(request);
+        Customer savedCustomer = customerService.changePassword(mappedCustomer.getEmail(),mappedCustomer.getPassword());
+        return new ResponseEntity<>(CustomerMapper.INSTANCE.modelToCustomerChangePasswordResponse(savedCustomer), HttpStatus.CREATED);
     }
 }
