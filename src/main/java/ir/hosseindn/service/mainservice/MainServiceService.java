fe2845b0804174be727d992cab80a8d5f6cb3208
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 19:50:57 +0330

    Service : MainService > create find all method in service

--- src/main/java/ir/hosseindn/service/mainservice/MainServiceService.java
+++ src/main/java/ir/hosseindn/service/mainservice/MainServiceService.java
@@ -1,10 +1,13 @@
 package ir.hosseindn.service.mainservice;
 
 import ir.hosseindn.exception.DuplicateInformationException;
+import ir.hosseindn.exception.NotFoundException;
 import ir.hosseindn.model.MainService;
 import ir.hosseindn.repository.mainservice.MainServiceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,4 +19,10 @@ public class MainServiceService {
             throw new DuplicateInformationException("A Main-Service with this name Already Exists!");
         return mainServiceRepository.save(mainService);
     }
+    public List<MainService> findAll() {
+        if (mainServiceRepository.findAll().isEmpty())
+            throw new NotFoundException("No Main-Service Found!");
+        return mainServiceRepository.findAll();
+    }
+
 }
