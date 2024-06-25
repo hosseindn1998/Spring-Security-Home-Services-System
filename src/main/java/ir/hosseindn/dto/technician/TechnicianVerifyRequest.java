eb57e2cc00db4379da253e883e46c201d04f9681
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 22:20:47 +0330

    dto : TechnicianVerifyRequest created

---
+++ src/main/java/ir/hosseindn/dto/technician/TechnicianVerifyRequest.java
@@ -0,0 +1,13 @@
+package ir.hosseindn.dto.technician;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public record TechnicianVerifyRequest(
+        @NotNull
+        @Column(unique = true)
+        @Email(message = "Email must be valid")
+        String email
+) {
+}
