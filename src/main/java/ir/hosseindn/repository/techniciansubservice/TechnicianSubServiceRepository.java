168f73c8be2033167ac0b0875fd1a4ff265c559a
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 11:05:37 +0330

    Repository : TechnicianSubServiceRepository > create method findAllTechnician by subService

--- src/main/java/ir/hosseindn/repository/techniciansubservice/TechnicianSubServiceRepository.java
+++ src/main/java/ir/hosseindn/repository/techniciansubservice/TechnicianSubServiceRepository.java
@@ -1,11 +1,18 @@
 package ir.hosseindn.repository.techniciansubservice;
 
+import ir.hosseindn.model.Technician;
 import ir.hosseindn.model.TechnicianSubService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 
 public interface TechnicianSubServiceRepository extends JpaRepository<TechnicianSubService, Long> {
+    @Query(value = "select ts.technician from TechnicianSubService ts where ts.subService.name=:subServiceName", nativeQuery = true)
+    List<Technician> findAllTechnicianBySubService(@Param("subServiceName") String subServiceName);
 
 }
