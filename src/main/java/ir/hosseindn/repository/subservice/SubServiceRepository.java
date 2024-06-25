e0df595f3157aca68e9268ba0ecacaa226199c14
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 20:16:15 +0330

    repository : create query for update basePrice and description

--- src/main/java/ir/hosseindn/repository/subservice/SubServiceRepository.java
+++ src/main/java/ir/hosseindn/repository/subservice/SubServiceRepository.java
@@ -1,7 +1,11 @@
 package ir.hosseindn.repository.subservice;
 
 import ir.hosseindn.model.SubService;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +13,8 @@ import java.util.Optional;
 @Repository
 public interface SubServiceRepository extends JpaRepository<SubService, Long> {
     Optional<SubService> findByName(String name);
-
+    @Modifying
+    @Transactional
+    @Query("update SubService s set s.basePrice=:newBasePrice ,s.description=:description where s.name=:name")
+    void updateByName(@Param("name") String name, @Param("newBasePrice") Long newBasePrice,@Param("description") String description);
 }
