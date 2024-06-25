81392f95d4691a064d531ba0da0adb73c79ef114
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 08:46:13 +0330

    Repository : MainServiceRepository > find by name method created

--- src/main/java/ir/hosseindn/repository/mainservice/MainServiceRepository.java
+++ src/main/java/ir/hosseindn/repository/mainservice/MainServiceRepository.java
@@ -4,8 +4,10 @@ import ir.hosseindn.model.MainService;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MainServiceRepository extends JpaRepository<MainService, Long> {
 
-
+Optional<MainService>findByName(String name);
 }
