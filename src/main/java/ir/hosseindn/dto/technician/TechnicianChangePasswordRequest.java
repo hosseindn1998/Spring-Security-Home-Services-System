26e6eb2196af273e39320c61ae6f830ca43e10bc
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 08:06:50 +0330

    dto : create dto for change password request

---
+++ src/main/java/ir/hosseindn/dto/technician/TechnicianChangePasswordRequest.java
@@ -0,0 +1,13 @@
+package ir.hosseindn.dto.technician;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
+public record TechnicianChangePasswordRequest(
+        @Email(message = "Email must be valid")
+        String email,
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8}$",message ="Password Not valid ")
+        String newPassword,
+        String confirmPassword
+) {
+}
