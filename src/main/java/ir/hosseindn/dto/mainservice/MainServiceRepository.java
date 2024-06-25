17015e513af8f6a279f7bfe38151bea366ffdcb3
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 08:37:35 +0330

    dto : created Main Service Save Request >change name to unique

--- src/main/java/ir/hosseindn/dto/mainservice/MainServiceRepository.java
+++
@@ -1,7 +0,0 @@
-package ir.hosseindn.dto.mainservice;
-
-import ir.hosseindn.model.MainService;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MainServiceRepository extends JpaRepository <MainService,Long> {
-}
--- src/main/java/ir/hosseindn/dto/mainservice/MainServiceSaveRequest.java
+++ src/main/java/ir/hosseindn/dto/mainservice/MainServiceSaveRequest.java
@@ -1,6 +1,9 @@
 package ir.hosseindn.dto.mainservice;
 
+import jakarta.persistence.Column;
+
 public record MainServiceSaveRequest(
+        @Column(unique = true)
         String name
 ) {
 }
