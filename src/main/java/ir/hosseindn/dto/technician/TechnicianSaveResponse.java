cc3b811274f2b0046b357a0a8838a318a577dded
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 08:02:27 +0330

    dto : dto for TechnicianSaveResponse created

---
+++ src/main/java/ir/hosseindn/dto/technician/TechnicianSaveResponse.java
@@ -0,0 +1,13 @@
+package ir.hosseindn.dto.technician;
+
+import java.time.LocalDate;
+
+public record TechnicianSaveResponse(
+        Long id,
+        String firstName,
+        String lastName,
+        String nationalCode,
+        String email,
+        LocalDate registeredDate
+) {
+}
