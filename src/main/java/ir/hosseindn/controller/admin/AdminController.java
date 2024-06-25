c78e0f62ff0a6556765361fa96061a0d545255b5
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 09:58:26 +0330

    Controller : AdminController > login method created

---
+++ src/main/java/ir/hosseindn/controller/admin/AdminController.java
@@ -0,0 +1,33 @@
+package ir.hosseindn.controller.admin;
+
+import ir.hosseindn.dto.admin.AdminLoginRequest;
+import ir.hosseindn.dto.admin.AdminLoginResponse;
+import ir.hosseindn.dto.customer.*;
+import ir.hosseindn.exception.NotValidInformation;
+import ir.hosseindn.mapper.admin.AdminMapper;
+import ir.hosseindn.mapper.customer.CustomerMapper;
+import ir.hosseindn.model.Admin;
+import ir.hosseindn.model.Customer;
+import ir.hosseindn.service.admin.AdminService;
+import ir.hosseindn.service.customer.CustomerService;
+import ir.hosseindn.utility.CustomValidations;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@Slf4j
+public class AdminController {
+    private final AdminService adminService;
+    @GetMapping("/admin-login")
+    public ResponseEntity<AdminLoginResponse> adminLogin(@Validated @RequestBody AdminLoginRequest request){
+        Admin mappedAdmin = AdminMapper.INSTANCE.adminLoginRequestToModel(request);
+        Admin loggedInAdmin=adminService.login(mappedAdmin.getEmail(),mappedAdmin.getPassword());
+        return new ResponseEntity<>(AdminMapper.INSTANCE.modelToAdminLoginResponse(loggedInAdmin),HttpStatus.FOUND);
+    }
+}
