dc534eea558d049e6058f3aeb57427591d40531a
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 19:39:21 +0330

    Mapper : technicianSubServiceMapper> create delete request and delete response methods

---
+++ src/main/java/ir/hosseindn/mapper/techniciansubservice/TechnicianSubServiceMapper.java
@@ -0,0 +1,23 @@
+package ir.hosseindn.mapper.techniciansubservice;
+
+import ir.hosseindn.dto.techniciansubservice.TechnicianSubServiceDeleteRequest;
+import ir.hosseindn.dto.techniciansubservice.TechnicianSubServiceDeleteResponse;
+import ir.hosseindn.dto.techniciansubservice.TechnicianSubServiceSaveRequest;
+import ir.hosseindn.dto.techniciansubservice.TechnicianSubServiceSaveResponse;
+import ir.hosseindn.model.TechnicianSubService;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface TechnicianSubServiceMapper {
+
+    TechnicianSubServiceMapper INSTANCE = Mappers.getMapper(TechnicianSubServiceMapper.class);
+
+    TechnicianSubService technicianSubServiceSaveRequestToModel(TechnicianSubServiceSaveRequest request);
+
+    TechnicianSubServiceSaveResponse modelToTechnicianSubServiceSaveResponse(TechnicianSubService technicianSubService);
+    TechnicianSubService technicianSubServiceDeleteRequestToModel(TechnicianSubServiceDeleteRequest request);
+
+    TechnicianSubServiceDeleteResponse modelToTechnicianSubServiceDeleteResponse(TechnicianSubService technicianSubService);
+
+}
