76903f2861ff9e0e73dd80be8a099311c3021f50
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-26 01:31:55 +0330

    Mapper : order mapper >create instance

--- src/main/java/ir/hosseindn/mapper/order/OrderMapper.java
+++ src/main/java/ir/hosseindn/mapper/order/OrderMapper.java
@@ -2,11 +2,14 @@ package ir.hosseindn.mapper.order;
 
 import ir.hosseindn.dto.order.OrderSaveRequest;
 import ir.hosseindn.dto.order.OrderSaveResponse;
+import ir.hosseindn.mapper.subservice.SubServiceMapper;
 import ir.hosseindn.model.Order;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface OrderMapper {
+    OrderMapper INSTANCE = Mappers.getMapper(OrderMapper.class);
     Order orderSaveRequestToModel(OrderSaveRequest request);
 
     OrderSaveResponse modelToOrderSaveResponse(Order order);
