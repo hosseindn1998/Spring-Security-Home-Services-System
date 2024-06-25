4ecf630877f610ff01328f902e829de59a27d887
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 19:51:38 +0330

    Controller : MainServiceController > create method for see Main Services

--- src/main/java/ir/hosseindn/controller/mainservice/MainServiceController.java
+++ src/main/java/ir/hosseindn/controller/mainservice/MainServiceController.java
@@ -1,5 +1,7 @@
 package ir.hosseindn.controller.mainservice;
 
+import ir.hosseindn.dto.mainservice.MainServiceFindAllRequest;
+import ir.hosseindn.dto.mainservice.MainServiceFindAllResponse;
 import ir.hosseindn.dto.mainservice.MainServiceSaveRequest;
 import ir.hosseindn.dto.mainservice.MainServiceSaveResponse;
 import ir.hosseindn.mapper.mainservice.MainServiceMapper;
@@ -10,9 +12,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +31,10 @@ public class MainServiceController {
         MainService mappedMainService = MainServiceMapper.INSTANCE.mainServiceSaveRequestToModel(request);
         MainService saveMainService = mainServiceService.save(mappedMainService);
         return new ResponseEntity<>(MainServiceMapper.INSTANCE.modelToMainServiceSaveResponse(saveMainService), HttpStatus.CREATED);
+    }
+    @GetMapping("/see-MainServices")
+    public ResponseEntity<List<MainService>> seeMainServices() {
+        List<MainService> mainServiceList = mainServiceService.findAll();
+        return new ResponseEntity<>(mainServiceList, HttpStatus.FOUND);
     }
 }
