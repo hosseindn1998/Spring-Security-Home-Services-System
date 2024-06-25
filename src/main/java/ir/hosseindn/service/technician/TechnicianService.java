e55f9fdecdb573ff1a8909bff3e7043b861251dc
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 08:08:23 +0330

    Service :technician > create service and register method

---
+++ src/main/java/ir/hosseindn/service/technician/TechnicianService.java
@@ -0,0 +1,38 @@
+package ir.hosseindn.service.technician;
+
+import ir.hosseindn.exception.DuplicateInformationException;
+import ir.hosseindn.exception.NotFoundException;
+import ir.hosseindn.exception.NotValidInformation;
+import ir.hosseindn.model.Technician;
+import ir.hosseindn.repository.technician.TechnicianRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@RequiredArgsConstructor
+public class TechnicianService {
+    private final TechnicianRepository technicianRepository;
+
+    public Technician register(Technician technician) {
+        if (technicianRepository.findByEmailOrNationalCode(technician.getEmail(), technician.getNationalCode()).isPresent())
+            throw new DuplicateInformationException("A Technician with this Email/National Code exist.");
+        technician.setRegisteredDate(LocalDate.now());
+        return technicianRepository.save(technician);
+    }
+
+    public Technician changePassword(String email, String newPassword) {
+        Technician technician = technicianRepository.findByEmail(email).orElseThrow(
+                () -> new NotFoundException("Technician with email :" + email + " Not found.")
+        );
+        technicianRepository.updatePassword(email, newPassword);
+        return technician;
+    }
+
+    public Technician login(String email, String Password) {
+        return technicianRepository.findByEmailAndPassword(email, Password).orElseThrow(
+                () -> new NotValidInformation("Email or Password is Incorrect")
+        );
+    }
+}
