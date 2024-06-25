4f3bfc3119849b3bed176bf596d631a66a15a269
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 22:35:23 +0330

    Repository : TechnicianRepository> query for update Technician Status

--- src/main/java/ir/hosseindn/repository/technician/TechnicianRepository.java
+++ src/main/java/ir/hosseindn/repository/technician/TechnicianRepository.java
@@ -1,6 +1,7 @@
 package ir.hosseindn.repository.technician;
 
 import ir.hosseindn.model.Technician;
+import ir.hosseindn.model.TechnicianStatus;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -23,5 +24,6 @@ public interface TechnicianRepository extends JpaRepository<Technician, Long> {
     @Transactional
     @Query("update Technician t set t.password=:password where t.email=:email")
     void updatePassword(@Param("email") String email, @Param("password") String password);
-
+    @Query("UPDATE Technician t set t.technicianStatus=:technicianStatus where t.email=:email")
+    void updateTechnicianStatus(@Param("technicianStatus") TechnicianStatus technicianStatus,@Param("email") String email);
 }
