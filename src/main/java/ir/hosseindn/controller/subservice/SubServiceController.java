27aac68cdc1d608158c03dd968319725bb292d14
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 09:36:59 +0330

    Controller : SubServiceController > add subservice method created

---
+++ src/main/java/ir/hosseindn/controller/subservice/SubServiceController.java
@@ -0,0 +1,30 @@
+package ir.hosseindn.controller.subservice;
+
+import ir.hosseindn.dto.subservice.SubServiceSaveRequest;
+import ir.hosseindn.dto.subservice.SubServiceSaveResponse;
+import ir.hosseindn.mapper.subservice.SubServiceMapper;
+import ir.hosseindn.model.MainService;
+import ir.hosseindn.model.SubService;
+import ir.hosseindn.service.subservice.SubServiceService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@Slf4j
+public class SubServiceController {
+    private final SubServiceService subServiceService;
+    @PostMapping("/add-SubService")
+    public ResponseEntity<SubServiceSaveResponse>addSubService(@Validated @RequestBody SubServiceSaveRequest request){
+        SubService mappedSubService= SubServiceMapper.INSTANCE.subServiceSaveRequestToModel(request);
+        SubService savedSubService=subServiceService.save(mappedSubService);
+        return new ResponseEntity<>(SubServiceMapper.INSTANCE.modelToSubServiceSaveResponse(savedSubService), HttpStatus.CREATED);
+    }
+}
