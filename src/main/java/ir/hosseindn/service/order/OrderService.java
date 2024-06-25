ff5f8091c74c1634fd6115f0b9c58ccaa50fd802
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-26 01:22:14 +0330

    Service :OrderService > save method created

---
+++ src/main/java/ir/hosseindn/service/order/OrderService.java
@@ -0,0 +1,28 @@
+package ir.hosseindn.service.order;
+
+import ir.hosseindn.exception.NotFoundException;
+import ir.hosseindn.model.Order;
+import ir.hosseindn.model.OrderStatus;
+import ir.hosseindn.model.SubService;
+import ir.hosseindn.repository.order.OrderRepository;
+import ir.hosseindn.service.subservice.SubServiceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final SubServiceService subServiceService;
+
+    public Order save(Order order) {
+        SubService foundedSubService = Optional.ofNullable(subServiceService.findByName(order.getSubservice())).orElseThrow(
+                () -> new NotFoundException("SubService not found")
+        );
+        order.setOrderStatus(OrderStatus.WAIT_FOR_TECHNICIAN_OFFER);
+        order.setSubservice(foundedSubService);
+        return orderRepository.save(order);
+    }
+}
