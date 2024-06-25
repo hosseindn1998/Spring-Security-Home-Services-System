2c01c0d4c6bb1f257b68764a8a9b298bb5fd4648
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 08:58:28 +0330

    Controller ; Main service controller : add Main Service method created

---
+++ src/main/java/ir/hosseindn/controller/mainservice/MainServiceController.java
@@ -0,0 +1,30 @@
+package ir.hosseindn.controller.mainservice;
+
+import ir.hosseindn.dto.mainservice.MainServiceSaveRequest;
+import ir.hosseindn.dto.mainservice.MainServiceSaveResponse;
+import ir.hosseindn.mapper.mainservice.MainServiceMapper;
+import ir.hosseindn.model.MainService;
+import ir.hosseindn.service.mainservice.MainServiceService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+@Slf4j
+public class MainServiceController {
+    private final MainServiceService mainServiceService;
+
+    @PostMapping("/add-MainService")
+    public ResponseEntity<MainServiceSaveResponse> addMainService(@Validated @RequestBody MainServiceSaveRequest request) {
+        MainService mappedMainService = MainServiceMapper.INSTANCE.mainServiceSaveRequestToModel(request);
+        MainService saveMainService = mainServiceService.save(mappedMainService);
+        return new ResponseEntity<>(MainServiceMapper.INSTANCE.modelToMainServiceSaveResponse(saveMainService), HttpStatus.CREATED);
+    }
+}
