4e5bacaaa2688424b37f356fb05097a5b0381118
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 08:05:01 +0330

    Repository: Technician> create find by email or nationalCode and find by email and query for update password

--- src/main/java/ir/hosseindn/repository/technician/TechnicianRepository.java
+++ src/main/java/ir/hosseindn/repository/technician/TechnicianRepository.java
@@ -1,14 +1,27 @@
 package ir.hosseindn.repository.technician;
 
 import ir.hosseindn.model.Technician;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface TechnicianRepository extends JpaRepository<Technician,Long> {
+public interface TechnicianRepository extends JpaRepository<Technician, Long> {
 
     Optional<Technician> findByEmailAndPassword(String email, String password);
+
+    Optional<Technician> findByEmailOrNationalCode(String email, String nationalCode);
+
+    Optional<Technician> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("update Technician t set t.password=:password where t.email=:email")
+    void updatePassword(@Param("email") String email, @Param("password") String password);
 
 }
