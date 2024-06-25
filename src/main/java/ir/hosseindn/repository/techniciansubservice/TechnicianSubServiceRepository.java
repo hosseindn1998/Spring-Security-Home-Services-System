a11149a76ece164b9ec28b49cd20aef1eb516a48
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-25 19:48:33 +0330

    Repository : TechnicianSubServiceRepository > create method for find technician Subs Service by sub service and find by sub-service

--- src/main/java/ir/hosseindn/repository/techniciansubservice/TechnicianSubServiceRepository.java
+++ src/main/java/ir/hosseindn/repository/techniciansubservice/TechnicianSubServiceRepository.java
@@ -1,5 +1,6 @@
 package ir.hosseindn.repository.techniciansubservice;
 
+import ir.hosseindn.model.SubService;
 import ir.hosseindn.model.Technician;
 import ir.hosseindn.model.TechnicianSubService;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,11 +9,14 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 
 public interface TechnicianSubServiceRepository extends JpaRepository<TechnicianSubService, Long> {
-    @Query(value = "select ts.technician from TechnicianSubService ts where ts.subService.name=:subServiceName", nativeQuery = true)
-    List<Technician> findAllTechnicianBySubService(@Param("subServiceName") String subServiceName);
+    @Query("select ts.technician from TechnicianSubService ts  where ts.subService.name=:subServiceName")
+    List<Technician> findTechnicianSubServiceBySubService(@Param("subServiceName") String subServiceName);
+    @Query("from TechnicianSubService ts where ts.technician=:technician And ts.subService=:subService")
+    Optional<TechnicianSubService>findBySubServiceAndTechnician(@Param("technician") Technician technician,@Param("subService") SubService subService);
 
 }
