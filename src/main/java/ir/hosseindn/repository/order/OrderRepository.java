f4ecb84e18c38350051646095a84acce7b438df6
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-26 09:54:08 +0330

    Repository : OrderRepository : create method for check order open to get Offer

--- src/main/java/ir/hosseindn/repository/order/OrderRepository.java
+++ src/main/java/ir/hosseindn/repository/order/OrderRepository.java
@@ -2,10 +2,16 @@ package ir.hosseindn.repository.order;
 
 import ir.hosseindn.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface OrderRepository extends JpaRepository<Order,Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
+    @Query("from Order o where o.id=:id and (o.orderStatus=0 or o.orderStatus=1)")
+    Optional<Order> isOpenToGetOffer(@Param("id") Long id);
 
 
 }
