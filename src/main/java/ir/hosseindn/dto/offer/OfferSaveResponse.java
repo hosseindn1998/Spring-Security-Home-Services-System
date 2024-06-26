6ddefd67872ef0ce44de055b5d6ff707d6f8bc2d
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-26 07:11:01 +0330

    dto:created OfferSaveResponse

---
+++ src/main/java/ir/hosseindn/dto/offer/OfferSaveResponse.java
@@ -0,0 +1,23 @@
+package ir.hosseindn.dto.offer;
+
+import ir.hosseindn.model.Order;
+import ir.hosseindn.model.Technician;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record OfferSaveResponse(
+        Long id,
+        Order odrer,
+        LocalDate dateOfOfferToStart,
+        @Min(0)
+        @NotNull
+        Long suggestPrice,
+        LocalDate dateOfOfferToDone,
+        @ManyToOne
+        Technician technician,
+        Boolean isAccepted
+) {
+}
