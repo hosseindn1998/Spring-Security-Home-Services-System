d83c6d802c522d4062677616f6e6c366b9e58a24
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-26 07:17:38 +0330

    Mapper : offer mapper created with offerSaveRequestToModel

---
+++ src/main/java/ir/hosseindn/mapper/offer/OfferMapper.java
@@ -0,0 +1,13 @@
+package ir.hosseindn.mapper.offer;
+
+import ir.hosseindn.dto.offer.OfferSaveRequest;
+import ir.hosseindn.dto.offer.OfferSaveResponse;
+import ir.hosseindn.model.Offer;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+@Mapper
+public interface OfferMapper {
+    OfferMapper INSTANCE= Mappers.getMapper(OfferMapper.class);
+    Offer offerSaveRequestToModel(OfferSaveRequest request);
+    OfferSaveResponse modelToOfferSaveResponse(Offer offer);
+}
