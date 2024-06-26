2b16bfaf66607b6eda21d7d48e79cc6a006a5ed3
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-26 13:02:48 +0330

    Service :OfferService> create method for list of offer of order that order by suggest price and technician rate

--- src/main/java/ir/hosseindn/service/offer/OfferService.java
+++ src/main/java/ir/hosseindn/service/offer/OfferService.java
@@ -1,5 +1,6 @@
 package ir.hosseindn.service.offer;
 
+import ir.hosseindn.exception.NotFoundException;
 import ir.hosseindn.exception.NotValidInformation;
 import ir.hosseindn.model.Offer;
 import ir.hosseindn.model.Order;
@@ -7,8 +8,15 @@ import ir.hosseindn.model.OrderStatus;
 import ir.hosseindn.repository.offer.OfferRepository;
 import ir.hosseindn.service.order.OrderService;
 import ir.hosseindn.service.subservice.SubServiceService;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +24,7 @@ public class OfferService {
     private final OfferRepository offerRepository;
     private final OrderService orderService;
     private final SubServiceService subServiceService;
+    private final EntityManager entityManager;
     public Offer save(Offer offer){
         Order foundedOrder = orderService.findById(offer.getId());
         if(!orderService.isOpenToGetOffer(offer.getOdrer().getId()))
@@ -28,5 +37,11 @@ public class OfferService {
             orderService.save(foundedOrder);
         }
         return offerRepository.save(offer);
+    }
+    public List<Offer>findAllByOrder(Order order){
+        List<Offer> offerList = offerRepository.findAllByOdrer(order);
+        if(offerList.isEmpty())
+            throw new NotFoundException(String.format("No offers for order id=%s found",order.getId()));
+        return offerList;
     }
 }
