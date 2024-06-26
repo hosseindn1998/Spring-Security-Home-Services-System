61f85920fb98cc1a19fe0cfa9f82b5d32f5d49ac
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-26 13:02:17 +0330

    Repository:Offer Repository > create query for list of offer of order that order by suggest price and technician rate

--- src/main/java/ir/hosseindn/repository/offer/OfferRepository.java
+++ src/main/java/ir/hosseindn/repository/offer/OfferRepository.java
@@ -1,10 +1,21 @@
 package ir.hosseindn.repository.offer;
 
 import ir.hosseindn.model.Offer;
+import ir.hosseindn.model.Order;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OfferRepository extends JpaRepository<Offer,Long> {
-
+    @Query("FROM Offer o where o.odrer=:order ORDER BY o.suggestPrice asc,o.technician.rate desc ")
+    List<Offer>findAllByOdrer(@Param("order")Order order);
 }
