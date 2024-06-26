f4aec0a7cd6ad0a081df12607e502a243268e5ad
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-26 18:28:08 +0330

    Repository : OrderRepository > create choose offer method

--- src/main/java/ir/hosseindn/repository/order/OrderRepository.java
+++ src/main/java/ir/hosseindn/repository/order/OrderRepository.java
@@ -1,5 +1,6 @@
 package ir.hosseindn.repository.order;
 
+import ir.hosseindn.model.Offer;
 import ir.hosseindn.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,7 +13,8 @@ import java.util.Optional;
 public interface OrderRepository extends JpaRepository<Order, Long> {
     @Query("from Order o where o.id=:id and (o.orderStatus=0 or o.orderStatus=1)")
     Optional<Order> isOpenToGetOffer(@Param("id") Long id);
-
+    @Query("update from Order o set o.choosedOffer=:offer where o.id=:id")
+    void chooseOffer(@Param("id")Long id,@Param("offer") Offer Offer);
 
 }
 
