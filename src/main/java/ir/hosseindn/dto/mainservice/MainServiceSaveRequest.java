34beddda68386de0d31eb717b010c05e4e1e150e
hosseindn1998 <hosseindn1998@gmail.com>
2024-06-27 05:01:56 +0330

    dto : MainServiceSaveRequest > improve validations

--- src/main/java/ir/hosseindn/dto/mainservice/MainServiceSaveRequest.java
+++ src/main/java/ir/hosseindn/dto/mainservice/MainServiceSaveRequest.java
@@ -1,9 +1,13 @@
 package ir.hosseindn.dto.mainservice;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record MainServiceSaveRequest(
         @Column(unique = true)
+        @Pattern(regexp = "^[A-Z](?=.{1,29}$)[A-Za-z]*(?:\\h+[A-Z][A-Za-z]*)*$",message = "Could be 1-29 char and contain only alphabet")
+        @NotNull
         String name
 ) {
 }
