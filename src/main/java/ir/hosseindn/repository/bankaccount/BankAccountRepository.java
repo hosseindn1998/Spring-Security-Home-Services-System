73a3da0930628f61206f42e1389f40a7f00dc503
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-05 00:04:11 +0330

    Bank Account > Repository and Service class and methods created for withdraw

---
+++ src/main/java/ir/hosseindn/repository/bankaccount/BankAccountRepository.java
@@ -0,0 +1,22 @@
+package ir.hosseindn.repository.bankaccount;
+
+import ir.hosseindn.model.BankAccount;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface BankAccountRepository extends JpaRepository<BankAccount,Long> {
+
+    Optional<BankAccount> findByCardNumberAndCvvAndMmAndYyAndPassword(String cardNumber, String cvv, String mm, String yy, String password);
+    @Modifying
+    @Transactional
+    @Query("update BankAccount b SET b.amount=(b.amount-:amount) where b.cardNumber=:cardNumber")
+    void withdraw(@Param("cardNumber") String cardNumber,@Param("amount") Long amount);
+    Optional<BankAccount>findByCardNumber(String cardNumber);
+}
---
+++ src/main/java/ir/hosseindn/service/bankaccount/BankAccountService.java
@@ -0,0 +1,27 @@
+package ir.hosseindn.service.bankaccount;
+
+import ir.hosseindn.exception.NotValidInformation;
+import ir.hosseindn.model.BankAccount;
+import ir.hosseindn.repository.bankaccount.BankAccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BankAccountService {
+    private final BankAccountRepository bankAccountRepository;
+
+    public BankAccount findIfValidAccountInformation(String cardNumber, String cvv, String mm, String yy, String password) {
+        return bankAccountRepository.findByCardNumberAndCvvAndMmAndYyAndPassword(cardNumber, cvv, mm, yy, password).orElseThrow(
+                () -> new NotValidInformation("Card information Not valid")
+        );
+    }
+    public BankAccount findByCardNumber(String cardNumber){
+        return bankAccountRepository.findByCardNumber(cardNumber).orElseThrow(
+                () -> new NotValidInformation(String.format("Card with Number %s Not found",cardNumber))
+        );
+    }
+    public void withdraw(String cardNumber,Long amount){
+        bankAccountRepository.withdraw(cardNumber, amount);
+    }
+}
