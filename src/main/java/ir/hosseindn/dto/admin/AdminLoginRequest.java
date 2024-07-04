6cfb8bb9fd2f52e9bbf6fc726f467aac96d537c0
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-04 21:01:18 +0330

    Model And Dto : admin > checked for validation move from model to dto

--- src/main/java/ir/hosseindn/dto/admin/AdminLoginRequest.java
+++ src/main/java/ir/hosseindn/dto/admin/AdminLoginRequest.java
@@ -6,7 +6,7 @@ import jakarta.validation.constraints.Pattern;
 public record AdminLoginRequest(
         @Email(message = "Email must be valid")
         String email,
-        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8}$", message = "Password Not valid ")
+
         String password
 ) {
 }
--- target/generated-sources/annotations/ir/hosseindn/mapper/admin/AdminMapperImpl.java
+++ target/generated-sources/annotations/ir/hosseindn/mapper/admin/AdminMapperImpl.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-07-02T04:29:43+0330",
+    date = "2024-07-04T07:11:59+0330",
     comments = "version: 1.5.2.Final, compiler: javac, environment: Java 17.0.9 (Amazon.com Inc.)"
 )
 public class AdminMapperImpl implements AdminMapper {
