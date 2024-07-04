3e4c8b4db3cfd9d190e0ed0a9e9c7b4d2db4ea71
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-05 00:09:12 +0330

    dto : technician save request> add valid path file for technician registration

--- src/main/java/ir/hosseindn/dto/technician/TechnicianSaveRequest.java
+++ src/main/java/ir/hosseindn/dto/technician/TechnicianSaveRequest.java
@@ -1,7 +1,10 @@
 package ir.hosseindn.dto.technician;
 
+import jakarta.validation.constraints.Pattern;
+
 public record TechnicianSaveRequest(
         TechnicianSaveRequestWithoutPath technician,
+        @Pattern(regexp = "^[^\\s]+:\\\\+[^\\s]+\\.(jpg|jpeg)$",message = "technician's image not found or not valid file path")
         String imagePath
 ) {
 }
