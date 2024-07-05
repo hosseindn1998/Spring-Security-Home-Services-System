b36739c075c9cea62e7038e0c1b7dca11269e1d1
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-05 06:16:26 +0330

    Controller : Order Controller >pay order From payment

--- src/main/java/ir/hosseindn/controller/order/OrderController.java
+++ src/main/java/ir/hosseindn/controller/order/OrderController.java
@@ -1,7 +1,6 @@
 package ir.hosseindn.controller.order;
 
 import ir.hosseindn.dto.order.*;
-import ir.hosseindn.dto.wallet.WalletPayResponse;
 import ir.hosseindn.exception.NotValidInformation;
 import ir.hosseindn.mapper.customer.CustomerMapper;
 import ir.hosseindn.mapper.offer.OfferMapper;
@@ -10,6 +9,7 @@ import ir.hosseindn.mapper.subservice.SubServiceMapper;
 import ir.hosseindn.model.*;
 import ir.hosseindn.service.offer.OfferService;
 import ir.hosseindn.service.order.OrderService;
+import ir.hosseindn.service.paymenttransaction.PaymentTransactionService;
 import ir.hosseindn.service.technician.TechnicianService;
 import ir.hosseindn.service.wallet.WalletService;
 import jakarta.validation.Valid;
@@ -17,11 +17,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.time.Duration;
 import java.time.LocalDateTime;
@@ -35,6 +36,7 @@ public class OrderController {
     private final OfferService offerService;
     private final TechnicianService technicianService;
     private final WalletService walletService;
+    private final PaymentTransactionService paymentTransactionService;
 
     @PostMapping("/add-order")
     public ResponseEntity<OrderSaveResponse> addOrder(@Valid @RequestBody OrderSaveRequest request) {
@@ -74,18 +76,19 @@ public class OrderController {
     public ResponseEntity<OrderChangeStatusResponse> orderStatusToDone(@Valid @RequestBody OrderChangeStatusRequest request) {
         Order mappedOrder = OrderMapper.INSTANCE.orderIdToModel(request.order());
         Order updatedOrder = orderService.changeOrderStatusToDone(mappedOrder);
-        if (LocalDateTime.now().isAfter(updatedOrder.getChoosedOffer().getDateOfOfferToDone())){
+        if (LocalDateTime.now().isAfter(updatedOrder.getChoosedOffer().getDateOfOfferToDone())) {
             long hours = Duration.between(LocalDateTime.now(), updatedOrder.getChoosedOffer().getDateOfOfferToDone())
                     .toHours();
             Technician technician = updatedOrder.getChoosedOffer().getTechnician();
-            technicianService.updateScores(technician.getId(),hours);
+            technicianService.updateScores(technician.getId(), hours);
         }
         return new ResponseEntity<>(OrderMapper.INSTANCE.modelToOrderChangeStatusResponse(updatedOrder), HttpStatus.OK);
     }
+
     @PatchMapping("/pay-order-from-wallet")
-    public ResponseEntity<PayOrderFromWalletResponse> payOrderFromWallet(@Valid @RequestBody PayOrderFromWalletRequest request){
-        Order order=orderService.findById(request.order().id());
-        if(order.getOrderStatus()!=OrderStatus.DONE)
+    public ResponseEntity<PayOrderFromWalletResponse> payOrderFromWallet(@Valid @RequestBody PayOrderFromWalletRequest request) {
+        Order order = orderService.findById(request.order().id());
+        if (order.getOrderStatus() != OrderStatus.DONE)
             throw new NotValidInformation("Only orders that status = 'done' access to pay");
         walletService.payFromWallet(order.getCustomer().getWallet(), order.getChoosedOffer().getTechnician().getWallet()
                 , order.getChoosedOffer().getSuggestPrice());
@@ -93,4 +96,42 @@ public class OrderController {
         order.setOrderStatus(OrderStatus.Paid);
         return new ResponseEntity<>(OrderMapper.INSTANCE.modelToPayOrderFromWalletResponse(order), HttpStatus.OK);
     }
+
+        @GetMapping("/pay-order-from-payment")
+    public ResponseEntity<PayOrderFromPaymentResponse> payOrderFromPayment(@Valid @RequestBody PayOrderFromPaymentRequest request){
+        Order order=orderService.findById(request.order().id());
+        if(order.getOrderStatus()!=OrderStatus.DONE)
+            throw new NotValidInformation("Only orders that status = 'done' access to pay");
+        Long paymentPrice = order.getChoosedOffer().getSuggestPrice();
+
+        orderService.changeOrderStatusToPaid(order);
+        order.setOrderStatus(OrderStatus.Paid);
+        return new ResponseEntity<>(OrderMapper.INSTANCE.modelToPayOrderFromPaymentResponse(order), HttpStatus.OK);
+    }
+
+//    @RequestMapping(value = "/payment",method = {RequestMethod.GET})
+//    public ModelAndView showForm() {
+//        return new ModelAndView("payment", "paymentTransaction", new PaymentTransaction());
+//    }
+//
+//    @RequestMapping(value = "/addPaymentTransaction",method = {RequestMethod.POST})
+//    public String submit(@Valid @ModelAttribute PaymentTransaction paymentTransaction,
+//                         BindingResult result, ModelMap model) {
+//        if (result.hasErrors()) {
+//            return "error";
+//        }
+//        model.addAttribute("id", paymentTransaction.getId());
+//        model.addAttribute("cardNumber", paymentTransaction.getCardNumber());
+//        model.addAttribute("cvv", paymentTransaction.getCvv());
+//        model.addAttribute("yy", paymentTransaction.getYy());
+//        model.addAttribute("mm", paymentTransaction.getMm());
+//        model.addAttribute("password", paymentTransaction.getPassword());
+//
+//        return "PaymentTransactionView";
+//    }
+//    @GetMapping("/example")
+//    public String exampleMethod(Model model) {
+//        model.addAttribute("exampleAttribute", "This is an example attribute");
+//        return "exampleView";
+//    }
 }
