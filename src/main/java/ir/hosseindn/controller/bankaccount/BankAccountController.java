718aab655906a1ab72036816b1d12c0012ee6dd5
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-05 06:11:15 +0330

    Controller :bank Account Controller :created

---
+++ src/main/java/ir/hosseindn/controller/bankaccount/BankAccountController.java
@@ -0,0 +1,25 @@
+package ir.hosseindn.controller.bankaccount;
+
+import ir.hosseindn.dto.bankaccount.BankAccountSaveRequest;
+import ir.hosseindn.mapper.bankaccount.BankAccountMapper;
+import ir.hosseindn.model.BankAccount;
+import ir.hosseindn.service.bankaccount.BankAccountService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Validated
+public class BankAccountController {
+    private final BankAccountService bankAccountService;
+    @PostMapping("add-bank-account")
+    public BankAccount save(@Valid @RequestBody BankAccountSaveRequest request){
+        BankAccount mappedBankAccount= BankAccountMapper.INSTANCE.bankAccountSaveRequestToModel(request);
+        BankAccount saved = bankAccountService.save(mappedBankAccount);
+        return saved;
+    }
+}
