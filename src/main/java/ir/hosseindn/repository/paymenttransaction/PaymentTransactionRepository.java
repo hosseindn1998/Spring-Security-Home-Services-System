38beeae1d6a0f64ad68820dd1ccc93ca51982f98
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-05 06:19:36 +0330

    Repository :Payment TransactionRepository interface and update captcha answer method created

---
+++ src/main/java/ir/hosseindn/repository/paymenttransaction/PaymentTransactionRepository.java
@@ -0,0 +1,16 @@
+package ir.hosseindn.repository.paymenttransaction;
+
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public interface PaymentTransactionRepository extends JpaRepository<ir.hosseindn.model.PaymentTransaction, Long> {
+    @Modifying
+    @Query("UPDATE PaymentTransaction p SET p.captchaAnswer=:newAnswer where p.id=:id")
+    void updateCaptchaAnswer(@Param("id") Long id,@Param("newAnswer") String newAnswer);
+}
