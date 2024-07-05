db23ab34fe82b4c8f6c50a0c325a48e9494bd0f4
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-05 06:23:20 +0330

    Service : Payment Transaction Service > update Captcha Answer method created

--- src/main/java/ir/hosseindn/service/paymenttransaction/PaymentTransactionService.java
+++ src/main/java/ir/hosseindn/service/paymenttransaction/PaymentTransactionService.java
@@ -1,5 +1,6 @@
 package ir.hosseindn.service.paymenttransaction;
 
+import ir.hosseindn.exception.NotFoundException;
 import ir.hosseindn.model.PaymentTransaction;
 import ir.hosseindn.repository.paymenttransaction.PaymentTransactionRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,9 @@ public class PaymentTransactionService {
 private final PaymentTransactionRepository paymentTransactionRepository;
     public PaymentTransaction save(PaymentTransaction paymentTransaction){
         return paymentTransactionRepository.save(paymentTransaction);
+    }
+    public void updateCaptchaAnswer(Long id,String newAnswer){
+        paymentTransactionRepository.updateCaptchaAnswer(id,newAnswer);
     }
 
 
