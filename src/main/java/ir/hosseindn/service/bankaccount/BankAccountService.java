be034401049193d1ca6ac8dcb226868d50c9b09d
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-19 03:04:07 +0330

    Service : BankAccountService > delete FindByCardNumber method and unused imports

--- src/main/java/ir/hosseindn/service/bankaccount/BankAccountService.java
+++ src/main/java/ir/hosseindn/service/bankaccount/BankAccountService.java
@@ -1,19 +1,9 @@
 package ir.hosseindn.service.bankaccount;
 
-import ir.hosseindn.exception.NotFoundException;
-import ir.hosseindn.exception.NotValidInformation;
 import ir.hosseindn.model.BankAccount;
-import ir.hosseindn.model.Order;
-import ir.hosseindn.model.OrderStatus;
-import ir.hosseindn.model.PaymentTransaction;
 import ir.hosseindn.repository.bankaccount.BankAccountRepository;
-import ir.hosseindn.service.order.OrderService;
-import ir.hosseindn.service.paymenttransaction.PaymentTransactionService;
-import ir.hosseindn.service.wallet.WalletService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
@@ -24,17 +14,11 @@ public class BankAccountService {
         return bankAccountRepository.existsByCardNumberAndCvvAndMmAndYyAndPassword(cardNumber, cvv, mm, yy, password);
     }
 
-    public BankAccount save(BankAccount bankAccount){
+    public BankAccount save(BankAccount bankAccount) {
         return bankAccountRepository.save(bankAccount);
     }
 
-    public BankAccount findByCardNumber(String cardNumber){
-        return bankAccountRepository.findByCardNumber(cardNumber).orElseThrow(
-                () -> new NotValidInformation(String.format("Card with Number %s Not found",cardNumber))
-        );
-    }
-
-    public void withdraw(String cardNumber,Long amount){
+    public void withdraw(String cardNumber, Long amount) {
         bankAccountRepository.withdraw(cardNumber, amount);
     }
 
