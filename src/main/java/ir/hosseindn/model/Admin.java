7d48216d4496e672b8ec18f44e57c37c4054f66b
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-18 06:10:27 +0330

    Model : admin class updated

--- src/main/java/ir/hosseindn/model/Admin.java
+++ src/main/java/ir/hosseindn/model/Admin.java
@@ -1,10 +1,10 @@
 package ir.hosseindn.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @NoArgsConstructor
 @Getter
@@ -14,6 +14,40 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 @ToString(callSuper = true)
 @Table(name = "admins")
-public class Admin extends Person {
+public class Admin extends User implements UserDetails {
+    @Enumerated(EnumType.STRING)
+    Role role;
+    @Id
+    private Long id;
+
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
