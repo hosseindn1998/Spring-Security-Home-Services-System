564e8945ab93ba7f08e9e5b366c634b97c06ce6c
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-18 06:14:58 +0330

    Model : technician class updated

--- src/main/java/ir/hosseindn/model/Technician.java
+++ src/main/java/ir/hosseindn/model/Technician.java
@@ -1,11 +1,11 @@
 package ir.hosseindn.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.List;
 
@@ -18,11 +18,9 @@ import java.util.List;
 @SuperBuilder
 @ToString(callSuper = true)
 @Table(name = "Technician")
-public class Technician extends Person {
-    @Enumerated
+public class Technician extends User implements UserDetails {
+    @Enumerated(EnumType.STRING)
     TechnicianStatus technicianStatus;
-    @Enumerated
-    Roles role;
     Double rate;
     Integer totalScores;
     Integer countScores;
@@ -41,6 +39,38 @@ public class Technician extends Person {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "technician", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
     @ToString.Exclude
     List<TechnicianSubService> technicianSubServices;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "technician")
+    List<Order> orders;
+    @Id
+    private Long id;
 
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
