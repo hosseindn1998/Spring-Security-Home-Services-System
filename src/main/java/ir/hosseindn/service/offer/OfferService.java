50ed8c660fc394f23a9e32a379a65a0bb74f253c
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-19 03:24:59 +0330

    Service : OfferService > create method addOfferByTechnician

--- src/main/java/ir/hosseindn/service/offer/OfferService.java
+++ src/main/java/ir/hosseindn/service/offer/OfferService.java
@@ -1,5 +1,6 @@
 package ir.hosseindn.service.offer;
 
+import ir.hosseindn.exception.DuplicateInformationException;
 import ir.hosseindn.exception.NotFoundException;
 import ir.hosseindn.exception.NotValidInformation;
 import ir.hosseindn.model.Offer;
@@ -7,6 +8,7 @@ import ir.hosseindn.model.Order;
 import ir.hosseindn.model.OrderStatus;
 import ir.hosseindn.repository.offer.OfferRepository;
 import ir.hosseindn.service.order.OrderService;
+import ir.hosseindn.service.technician.TechnicianService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,12 +19,16 @@ import java.util.List;
 public class OfferService {
     private final OfferRepository offerRepository;
     private final OrderService orderService;
+    private final TechnicianService technicianService;
     public Offer save(Offer offer){
         Order foundedOrder = orderService.findById(offer.getOdrer().getId());
         if(!orderService.isOpenToGetOffer(offer.getOdrer().getId()))
             throw new NotValidInformation("this order is close to get offer");
         if(offer.getSuggestPrice()<foundedOrder.getSubservice().getBasePrice())
             throw new NotValidInformation("Technician suggest-price can't be lower than base-price");
+        if(orderService.isExistByIdAndTechnician(foundedOrder.getId(),offer.getTechnician()))
+            throw new DuplicateInformationException("A offer for this technician exists for order");
+
         if(foundedOrder.getOffers()==null){
             foundedOrder.setOrderStatus(OrderStatus.WAIT_FOR_CHOOSE_TECHNICIAN);
             offer.setOdrer(foundedOrder);
@@ -31,9 +37,14 @@ public class OfferService {
         offer.setIsAccepted(false);
         return offerRepository.save(offer);
     }
+    public Offer addOfferByTechnician(Offer mappedOffer,Long technicianId,Long orderId){
+        mappedOffer.setTechnician(technicianService.findById(technicianId));
+        mappedOffer.setOdrer(orderService.findById(orderId));
+        return save(mappedOffer);
+    }
     public List<Offer>findAllByOrder(Long order){
         Order foundOrder=orderService.findById(order);
-        List<Offer> offerList = offerRepository.customQuery(foundOrder.getId());
+        List<Offer> offerList = offerRepository.offerListSorted(foundOrder.getId());
         if(offerList.isEmpty())
             throw new NotFoundException(String.format("No offers for order id=%s found",order));
         return offerList;
