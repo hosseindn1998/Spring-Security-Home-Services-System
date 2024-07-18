543057ab36f0ba3f5094930dc7c91a06b3894d7e
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-19 02:36:56 +0330

    Repository : OrderRepository > create method for seeAllbysubservice and see all by customerEmail

--- src/main/java/ir/hosseindn/repository/order/OrderRepository.java
+++ src/main/java/ir/hosseindn/repository/order/OrderRepository.java
@@ -1,8 +1,8 @@
 package ir.hosseindn.repository.order;
 
-import ir.hosseindn.model.Offer;
 import ir.hosseindn.model.Order;
 import ir.hosseindn.model.OrderStatus;
+import ir.hosseindn.model.Technician;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -10,13 +10,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 @Transactional
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    @Query("from Order o where o.id=:id and (o.orderStatus=0 or o.orderStatus=1)")
+    @Query("from Order o where o.id=:id and (o.orderStatus='WAIT_FOR_TECHNICIAN_OFFER' or o.orderStatus='WAIT_FOR_CHOOSE_TECHNICIAN')")
     Optional<Order> isOpenToGetOffer(@Param("id") Long id);
+    @Query("FROM Order o where o.subservice.name=:subServiceName AND " +
+            "(o.orderStatus='WAIT_FOR_TECHNICIAN_OFFER' or o.orderStatus='WAIT_FOR_CHOOSE_TECHNICIAN')")
+    List<Order> seeAllBySubService(String subServiceName);
+    @Query("FROM Order o where o.customer.email=:email")
+    List<Order> seeAllByCustomerEmail(String email);
     @Modifying
     @Query("update Order o set o.choosedOffer.id=:offerId where o.id=:id")
     void chooseOffer(@Param("id")Long id,@Param("offerId") Long OfferId);
