6640135642f41bd747a18857585b4423ada4c44d
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-19 02:23:11 +0330

    Repository : ConfirmationTokenRepository > create method find by token and update token at

---
+++ src/main/java/ir/hosseindn/repository/confirmationtoken/ConfirmationTokenRepository.java
@@ -0,0 +1,25 @@
+package ir.hosseindn.repository.confirmationtoken;
+
+import ir.hosseindn.model.ConfirmationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Repository
+@Transactional(readOnly = true)
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
+
+    Optional<ConfirmationToken> findByToken(String token);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE ConfirmationToken c SET c.confirmedAt =:confirmdAt WHERE c.token =:token")
+    void updateConfirmedAt(@Param("token")String token,
+                          @Param("confirmdAt") LocalDateTime confirmedAt);
+}
