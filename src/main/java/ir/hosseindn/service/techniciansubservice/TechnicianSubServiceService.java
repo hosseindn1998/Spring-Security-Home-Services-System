24acb700f1eda258a62d3e51086f03af318d64f2
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-19 04:04:16 +0330

    Service : TechnicianSubServiceService >update method save and delete

--- src/main/java/ir/hosseindn/service/techniciansubservice/TechnicianSubServiceService.java
+++ src/main/java/ir/hosseindn/service/techniciansubservice/TechnicianSubServiceService.java
@@ -2,30 +2,44 @@ package ir.hosseindn.service.techniciansubservice;
 
 import ir.hosseindn.exception.DuplicateInformationException;
 import ir.hosseindn.exception.NotFoundException;
+import ir.hosseindn.model.Order;
+import ir.hosseindn.model.SubService;
 import ir.hosseindn.model.TechnicianSubService;
 import ir.hosseindn.repository.techniciansubservice.TechnicianSubServiceRepository;
+import ir.hosseindn.service.order.OrderService;
 import ir.hosseindn.service.subservice.SubServiceService;
 import ir.hosseindn.service.technician.TechnicianService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TechnicianSubServiceService {
     private final TechnicianSubServiceRepository technicianSubServiceRepository;
+    private final OrderService orderService;
+    private final TechnicianService technicianService;
+    private final SubServiceService subServiceService;
 
-    public TechnicianSubService save(TechnicianSubService technicianSubService) {
-        if (technicianSubServiceRepository.findBySubServiceAndTechnician(technicianSubService.getTechnician(), technicianSubService.getSubService()).isPresent())
+
+    public TechnicianSubService save(Long technicianId, Long subServiceId) {
+        if (technicianSubServiceRepository.findBySubServiceAndTechnician(technicianId, subServiceId).isPresent())
             throw new DuplicateInformationException("this technician already has relation by subService");
+        TechnicianSubService technicianSubService = TechnicianSubService.builder()
+                .subService(subServiceService.findById(subServiceId))
+                .technician(technicianService.findById(technicianId))
+                .build();
         return technicianSubServiceRepository.save(technicianSubService);
     }
-    public void delete(TechnicianSubService technicianSubService) {
-        if (technicianSubServiceRepository.findBySubServiceAndTechnician(technicianSubService.getTechnician(), technicianSubService.getSubService()).isEmpty())
+    public String delete(Long technicianId, Long subServiceId) {
+        if (technicianSubServiceRepository.findBySubServiceAndTechnician(technicianId, subServiceId).isEmpty())
             throw new NotFoundException("this technician already hasn't relation by subService");
-        technicianSubServiceRepository.deleteById(technicianSubService.getTechnician().getId()
-                ,technicianSubService.getSubService().getId());
+        technicianSubServiceRepository.deleteById(technicianId, subServiceId);
+        return String.format("relation between technician id %s Sub-service id %s deleted",technicianId,subServiceId);
     }
+
+
 
 }
