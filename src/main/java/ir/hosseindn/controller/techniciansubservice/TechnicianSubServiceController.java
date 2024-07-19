0aa0caa7229dc3eab7c25a9d67119eca657813d0
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-19 05:37:17 +0330

    Controller : TechnicianSubServiceController > cut codes from controller to service methods

--- src/main/java/ir/hosseindn/controller/techniciansubservice/TechnicianSubServiceController.java
+++ src/main/java/ir/hosseindn/controller/techniciansubservice/TechnicianSubServiceController.java
@@ -1,14 +1,9 @@
 package ir.hosseindn.controller.techniciansubservice;
 
 import ir.hosseindn.dto.techniciansubservice.TechnicianSubServiceDeleteRequest;
-import ir.hosseindn.dto.techniciansubservice.TechnicianSubServiceDeleteResponse;
 import ir.hosseindn.dto.techniciansubservice.TechnicianSubServiceSaveRequest;
 import ir.hosseindn.dto.techniciansubservice.TechnicianSubServiceSaveResponse;
-import ir.hosseindn.mapper.subservice.SubServiceMapper;
-import ir.hosseindn.mapper.technician.TechnicianMapper;
 import ir.hosseindn.mapper.techniciansubservice.TechnicianSubServiceMapper;
-import ir.hosseindn.model.SubService;
-import ir.hosseindn.model.Technician;
 import ir.hosseindn.model.TechnicianSubService;
 import ir.hosseindn.service.techniciansubservice.TechnicianSubServiceService;
 import jakarta.validation.Valid;
@@ -16,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,30 +25,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class TechnicianSubServiceController {
     private final TechnicianSubServiceService technicianSubServiceService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/add-technician-to-subservice")
     public ResponseEntity<TechnicianSubServiceSaveResponse> addTechnicianToSubService(@Valid @RequestBody TechnicianSubServiceSaveRequest request) {
-        Technician technician = TechnicianMapper.INSTANCE.technicianIdToModel(request.technicianId());
-        SubService subService = SubServiceMapper.INSTANCE.subServiceIdToModel(request.subServiceId());
-        TechnicianSubService mappedTechnicianSubService = TechnicianSubService.builder()
-                .subService(subService)
-                .technician(technician)
-                .build();
-        TechnicianSubService savedTechnicianSubService = technicianSubServiceService.save(mappedTechnicianSubService);
+        TechnicianSubService savedTechnicianSubService = technicianSubServiceService.save(request.technicianId(), request.subServiceId());
         return new ResponseEntity<>(TechnicianSubServiceMapper.INSTANCE.modelToTechnicianSubServiceSaveResponse(savedTechnicianSubService)
                 , HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/delete-technician-from-subservice")
-    public ResponseEntity<TechnicianSubServiceDeleteResponse> deleteTechnicianFromSubService(@Valid @RequestBody TechnicianSubServiceDeleteRequest request) {
-        Technician technician = TechnicianMapper.INSTANCE.technicianIdToModel(request.technician());
-        SubService subService = SubServiceMapper.INSTANCE.subServiceIdToModel(request.subService());
-        TechnicianSubService mappedTechnicianSubService = TechnicianSubService.builder()
-                .subService(subService)
-                .technician(technician)
-                .build();
-        technicianSubServiceService.delete(mappedTechnicianSubService);
-        return new ResponseEntity<>(TechnicianSubServiceMapper.INSTANCE.modelToTechnicianSubServiceDeleteResponse(mappedTechnicianSubService), HttpStatus.OK);
+    public ResponseEntity<String> deleteTechnicianFromSubService(@Valid @RequestBody TechnicianSubServiceDeleteRequest request) {
+        String result = technicianSubServiceService.delete(request.technicianId(), request.subServiceId());
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
-
 
 }
