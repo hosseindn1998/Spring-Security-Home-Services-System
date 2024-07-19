ca976c77de69fa5d17caf3151c570f971b005876
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-19 03:59:45 +0330

    Service : TechnicianService > update register method for role set and password encoding,method checkConfirmationAndChangePassword,findByEmail,ChangeStatusToWaitForVerify,findByCriteria updated

--- src/main/java/ir/hosseindn/service/technician/TechnicianService.java
+++ src/main/java/ir/hosseindn/service/technician/TechnicianService.java
@@ -1,71 +1,84 @@
 package ir.hosseindn.service.technician;
 
+import ir.hosseindn.dto.technician.TechnicianChangePasswordRequest;
 import ir.hosseindn.dto.user.UserCriteriaItems;
 import ir.hosseindn.exception.DuplicateInformationException;
 import ir.hosseindn.exception.NotFoundException;
 import ir.hosseindn.exception.NotValidInformation;
-import ir.hosseindn.model.Roles;
-import ir.hosseindn.model.Technician;
-import ir.hosseindn.model.TechnicianStatus;
+import ir.hosseindn.model.Order;
+import ir.hosseindn.model.*;
 import ir.hosseindn.repository.technician.TechnicianRepository;
+import ir.hosseindn.service.confirmationservice.ConfirmationTokenService;
+import ir.hosseindn.service.user.UserService;
 import ir.hosseindn.utility.CustomValidations;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.criteria.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class TechnicianService {
     private final TechnicianRepository technicianRepository;
-    @Autowired
+    private final PasswordEncoder passwordEncoder;
+    private final ConfirmationTokenService confirmationTokenService;
+    private final UserService userService;
+    @PersistenceContext
     EntityManager entityManager;
 
-    public Technician register(Technician technician, String fileAddressAndName) throws IOException {
+    public Technician register(Technician technician, MultipartFile file) throws IOException {
         if (CustomValidations.isNotValidIranianNationalCode(technician.getNationalCode()))
             throw new NotValidInformation("National Code is Not valid");
-        if (technicianRepository.findByEmailOrNationalCode(technician.getEmail(), technician.getNationalCode()).isPresent()) {
-            throw new DuplicateInformationException("A Technician with this Email/National Code exist.");
-        }
-        if (!CustomValidations.isValidPathFile(fileAddressAndName))
-            throw new NotValidInformation("file address not valid ");
-        Path path = Paths.get(fileAddressAndName);
-        if (Files.size(path) > (300 * 1024))
+        if (userService.isExistEmail(technician.getEmail()))
+            throw new DuplicateInformationException("A USER with this Email exist.");
+        if (file.isEmpty())
+            throw new NotValidInformation("File is empty.");
+        if (!Objects.requireNonNull(file.getOriginalFilename()).endsWith(".jpg"))
+            throw new NotValidInformation("file must be jpg");
+        if (file.getSize() > (300 * 1024))
             throw new NotValidInformation("File must be les than 300 KB");
-        technician.setAvatar(Files.readAllBytes(path));
+        technician.setAvatar(file.getBytes());
         technician.setRegisteredDate(LocalDateTime.now());
         technician.setTechnicianStatus(TechnicianStatus.NEW_TECHNICIAN);
         technician.setTotalScores(0);
         technician.setCountScores(0);
         technician.setIsActive(Boolean.TRUE);
         technician.setRate(0.0);
-        technician.setRole(Roles.TECHNICIAN);
-        return technicianRepository.save(technician);
+        technician.setEnabled(Boolean.FALSE);
+        technician.setPassword(passwordEncoder.encode(technician.getPassword()));
+        technician.setRole(Role.ROLE_TECHNICIAN);
+        technician.setWallet(new Wallet(0L));
+        Technician savedTechnician = technicianRepository.save(technician);
+        confirmationTokenService.generateConfirmationToken(technician);
+        return savedTechnician;
     }
 
     public Technician update(Technician technician) {
         return technicianRepository.save(technician);
     }
 
+    public Technician checkConfirmationAndChangePassword(TechnicianChangePasswordRequest request) {
+        if (!request.password().equals(request.confirmPassword()))
+            throw new NotValidInformation("new password must be match by confirm");
+        return changePassword(request.email(), request.password());
+    }
+
     public Technician changePassword(String email, String newPassword) {
-        Technician technician = technicianRepository.findByEmail(email).orElseThrow(
-                () -> new NotFoundException("Technician with email :" + email + " Not found.")
-        );
+        Technician technician = findByEmail(email);
         technicianRepository.updatePassword(email, newPassword);
         return technician;
     }
@@ -88,6 +101,16 @@ public class TechnicianService {
         );
     }
 
+    public Technician findByEmail(String email) {
+        return technicianRepository.findByEmail(email).orElseThrow(
+                () -> new NotValidInformation("Email or Password is Incorrect")
+        );
+    }
+
+    public void changeStatusToWaitForVerify(String email) {
+        technicianRepository.updateTechnicianStatus(TechnicianStatus.WAIT_FOR_VERIFY, email);
+    }
+
     public Technician changeStatusToVerify(Technician technician) {
         Technician founded = technicianRepository.findByEmail(technician.getEmail()).orElseThrow(
                 () -> new NotFoundException("Technician with email :" + technician.getEmail() + " Not found."));
@@ -107,8 +130,6 @@ public class TechnicianService {
         Root<Technician> root = technicianQuery.from(Technician.class);
         List<Predicate> predicates = new ArrayList<>();
 
-        if (userCriteriaItems.role() != null)
-            predicates.add(builder.equal(root.get("role"), userCriteriaItems.role()));
         if (userCriteriaItems.id() != null)
             predicates.add(builder.equal(root.get("id"), userCriteriaItems.id()));
         if (userCriteriaItems.email() != null)
@@ -121,8 +142,10 @@ public class TechnicianService {
             predicates.add(builder.equal(root.get("nationalCode"), userCriteriaItems.nationalCode()));
         if (userCriteriaItems.password() != null)
             predicates.add(builder.equal(root.get("password"), userCriteriaItems.password()));
-        if (userCriteriaItems.registeredDate() != null)
-            predicates.add(builder.equal(root.get("registeredDate"), userCriteriaItems.registeredDate()));
+        if (userCriteriaItems.registeredDateStart() != null)
+            predicates.add(builder.greaterThanOrEqualTo(root.get("registeredDate"), userCriteriaItems.registeredDateStart()));
+        if (userCriteriaItems.registeredDateEnd() != null)
+            predicates.add(builder.lessThanOrEqualTo(root.get("registeredDate"), userCriteriaItems.registeredDateEnd()));
         if (userCriteriaItems.technicianStatus() != null)
             predicates.add(builder.equal(root.get("technicianStatus"), userCriteriaItems.technicianStatus()));
         if (userCriteriaItems.rate() != null)
@@ -134,22 +157,75 @@ public class TechnicianService {
         if (userCriteriaItems.isActive() != null)
             predicates.add(builder.equal(root.get("isActive"), userCriteriaItems.isActive()));
 
+        if (userCriteriaItems.walletId() != null) {
+            Join<Technician, Wallet> technicianWalletJoin = root.join("wallet", JoinType.INNER);
+            predicates.add(builder.equal(technicianWalletJoin.get("id"), userCriteriaItems.walletId()));
+        }
+
+        if (userCriteriaItems.subServiceName() != null) {
+            Join<Technician, TechnicianSubService> technicianTechnicianSubServiceJoin = root.join("technicianSubServices", JoinType.INNER);
+            Join<TechnicianSubService, SubService> technicianSubServiceSubServiceJoin = technicianTechnicianSubServiceJoin.join("subService", JoinType.LEFT);
+            predicates.add(builder.equal(technicianSubServiceSubServiceJoin.get("name"), userCriteriaItems.subServiceName()));
+        }
+
 
         technicianQuery.where(builder.and(predicates.toArray(predicates.toArray(new Predicate[]{}))));
 
-        return entityManager.createQuery(technicianQuery).getResultList();
+        List<Technician> list1 = entityManager.createQuery(technicianQuery).getResultList();
+        List<Technician> list2 = new ArrayList<>();
+        if (userCriteriaItems.countRequests() != null) {
+            CriteriaQuery<Object[]> orderQuery = builder.createQuery(Object[].class);
+            Root<Offer> rootOrder = orderQuery.from(Offer.class);
+            orderQuery.multiselect(
+                    rootOrder.get("technician"),
+                    builder.count(rootOrder)
+            );
+            orderQuery.groupBy(rootOrder.get("technician"));
+            orderQuery.having(builder.gt(builder.count(rootOrder), userCriteriaItems.countRequests()));
+            List<Object[]> results = entityManager.createQuery(orderQuery).getResultList();
+            for (Object[] result : results) {
+                Technician technician = (Technician) result[0];
+                list2.add(technician);
+            }
+            list1.retainAll(list2);
+            return list1;
+        }
+        if (userCriteriaItems.countDoneOrders() != null) {
+            CriteriaQuery<Object[]> orderQuery = builder.createQuery(Object[].class);
+            Root<Order> rootOrder = orderQuery.from(Order.class);
+            Predicate orderStatusPredicate = builder.equal(rootOrder.get("orderStatus"), OrderStatus.DONE);
+            orderQuery.multiselect(
+                    rootOrder.get("technician"),
+                    builder.count(rootOrder)
+            );
+            orderQuery.where(orderStatusPredicate);
+            orderQuery.groupBy(rootOrder.get("technician"));
+            orderQuery.having(builder.gt(builder.count(rootOrder), userCriteriaItems.countDoneOrders()));
+            List<Object[]> results = entityManager.createQuery(orderQuery).getResultList();
+            for (Object[] result : results) {
+                Technician technician = (Technician) result[0];
+                list2.add(technician);
+            }
+            list1.retainAll(list2);
+            return list1;
+        }
+
+        return list1;
     }
 
-    public void fetchAvatarFile(Long technicianId, String fileAddressForSave) {
+    public String fetchAvatarFile(Long technicianId, String fileAddressForSave) {
         if (!CustomValidations.isValidPathFile(fileAddressForSave))
             throw new NotValidInformation("File address Not valid");
         Technician technician = findById(technicianId);
-        byte[] avatar = technician.getAvatar();
-        try (FileOutputStream fos = new FileOutputStream(fileAddressForSave)) {
-            fos.write(avatar);
-            log.info("file added successfully");
+        Path path = Paths.get(fileAddressForSave);
+        try (FileOutputStream fos = new FileOutputStream(path.toString())) {
+            fos.write(technician.getAvatar());
+            return "fetch successfully";
         } catch (IOException e) {
             log.error(e.getMessage());
+            return "failed";
         }
     }
+
+
 }
