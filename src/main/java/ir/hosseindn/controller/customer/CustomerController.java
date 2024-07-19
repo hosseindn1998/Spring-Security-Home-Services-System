28689fdf656bc12f0b24ab613330669dfd8c5578
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-19 04:42:21 +0330

    Controller : CustomerController > create api for customer-order-history and api for customer-wallet

--- src/main/java/ir/hosseindn/controller/customer/CustomerController.java
+++ src/main/java/ir/hosseindn/controller/customer/CustomerController.java
@@ -1,18 +1,23 @@
 package ir.hosseindn.controller.customer;
 
 import ir.hosseindn.dto.customer.*;
-import ir.hosseindn.exception.NotValidInformation;
+import ir.hosseindn.dto.order.SeeCustomerOrdersResponse;
 import ir.hosseindn.mapper.customer.CustomerMapper;
+import ir.hosseindn.mapper.order.OrderMapper;
 import ir.hosseindn.model.Customer;
+import ir.hosseindn.model.Order;
 import ir.hosseindn.service.customer.CustomerService;
-import ir.hosseindn.utility.CustomValidations;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,19 +28,16 @@ public class CustomerController {
 
     @PostMapping("/customer-register")
     public ResponseEntity<CustomerSaveResponse> customerRegister(@Valid @RequestBody CustomerSaveRequest request) {
-        if (CustomValidations.isNotValidIranianNationalCode(request.nationalCode()))
-            throw new NotValidInformation("National Code is Not valid");
         Customer mappedCustomer = CustomerMapper.INSTANCE.customerSaveRequestToModel(request);
         Customer savedCustomer = customerService.register(mappedCustomer);
         return new ResponseEntity<>(CustomerMapper.INSTANCE.modelToUserSaveResponse(savedCustomer), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     @PatchMapping("/customer-changePassword")
-    public ResponseEntity<CustomerChangePasswordResponse> customerChangePassword(@Valid @RequestBody CustomerChangePasswordRequest request) {
-        if (!request.password().equals(request.confirmPassword()))
-            throw new NotValidInformation("new password must be match by confirm");
-        Customer mappedCustomer = CustomerMapper.INSTANCE.customerChangePasswordRequestToModel(request);
-        Customer savedCustomer = customerService.changePassword(mappedCustomer.getEmail(), mappedCustomer.getPassword());
+    public ResponseEntity<CustomerChangePasswordResponse> customerChangePassword(
+            @Valid @RequestBody CustomerChangePasswordRequest request) {
+        Customer savedCustomer = customerService.changePassword(request.email(), request.password(), request.confirmPassword());
         return new ResponseEntity<>(CustomerMapper.INSTANCE.modelToCustomerChangePasswordResponse(savedCustomer), HttpStatus.OK);
     }
 
@@ -45,4 +47,15 @@ public class CustomerController {
         Customer loggedInCustomer = customerService.login(mappedCustomer.getEmail(), mappedCustomer.getPassword());
         return new ResponseEntity<>(CustomerMapper.INSTANCE.modelToCustomerLoginResponse(loggedInCustomer), HttpStatus.FOUND);
     }
+    @GetMapping("/customer-order-history")
+    public ResponseEntity<List<SeeCustomerOrdersResponse>> customerOrderHistory(@Valid @RequestParam String orderStatus, Principal principal) {
+        List<Order> orderList = customerService.ordersHistory(principal.getName(), orderStatus);
+        return new ResponseEntity<>(OrderMapper.INSTANCE.modelListToSeeCustomerOrdersResponse(orderList), HttpStatus.FOUND);
+    }
+
+    @GetMapping("/customer-wallet")
+    public ResponseEntity<Long> customerSeeWallet(Principal principal) {
+        return new ResponseEntity<>(customerService.getWalletAmount(principal.getName()), HttpStatus.FOUND);
+    }
+
 }
