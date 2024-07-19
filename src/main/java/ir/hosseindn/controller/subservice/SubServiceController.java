cf4bede3b39594a3e05217cfb310874b1a438b0c
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-19 05:20:08 +0330

    Controller : SubServiceController > add PreAuthorize anotation

--- src/main/java/ir/hosseindn/controller/subservice/SubServiceController.java
+++ src/main/java/ir/hosseindn/controller/subservice/SubServiceController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,16 +22,16 @@ import java.util.List;
 @Slf4j
 public class SubServiceController {
     private final SubServiceService subServiceService;
-    private final MainServiceService mainServiceService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/add-SubService")
     public ResponseEntity<SubServiceSaveResponse> addSubService(@Valid @RequestBody SubServiceSaveRequest request) {
-        SubService mappedSubService = SubServiceMapper.INSTANCE.subServiceWithoutMainServiceSaveRequestToModel(request.subService());
-        mappedSubService.setMainService(mainServiceService.findById(request.mainServiceId()));
-        SubService savedSubService = subServiceService.save(mappedSubService);
+        SubService mappedSubService = SubServiceMapper.INSTANCE.subServiceSaveRequestToModel(request);
+        SubService savedSubService = subServiceService.saveRequest(mappedSubService, request.mainServiceId());
         return new ResponseEntity<>(SubServiceMapper.INSTANCE.modelToSubServiceSaveResponse(savedSubService), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PatchMapping("/update-SubService")
     public ResponseEntity<SubServiceUpdateResponse> updateSubService(@Valid @RequestBody SubServiceUpdateRequest request) {
         SubService mappedSubService = SubServiceMapper.INSTANCE.subServiceUpdateRequestToModel(request);
@@ -38,12 +39,10 @@ public class SubServiceController {
         return new ResponseEntity<>(SubServiceMapper.INSTANCE.modelToSubServiceUpdateResponse(savedSubService), HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/see-sub-services")
     public ResponseEntity<List<SubServiceFindAllResponse>> seeSubServices() {
         List<SubService> subServiceList = subServiceService.findAll();
-        ;
-        return new ResponseEntity<>(subServiceList.stream()
-                .map(SubServiceMapper.INSTANCE::modelToSubServiceFindAllResponse)
-                .toList(), HttpStatus.FOUND);
+        return new ResponseEntity<>(SubServiceMapper.INSTANCE.modelListToSubServiceFindAllResponseList(subServiceList), HttpStatus.FOUND);
     }
 }
