c2b0a816ba4c1c9c7bb815744dd96ae5faac2790
hosseindn1998 <hosseindn1998@gmail.com>
2024-07-19 05:28:09 +0330

    Controller : TechnicianController > create api for see-rate , technician-wallet ,technician-order-history

--- src/main/java/ir/hosseindn/controller/technician/TechnicianController.java
+++ src/main/java/ir/hosseindn/controller/technician/TechnicianController.java
@@ -1,19 +1,25 @@
 package ir.hosseindn.controller.technician;
 
+import ir.hosseindn.dto.order.SeeTechnicianOrdersResponse;
 import ir.hosseindn.dto.technician.*;
-import ir.hosseindn.exception.NotValidInformation;
+import ir.hosseindn.mapper.order.OrderMapper;
 import ir.hosseindn.mapper.technician.TechnicianMapper;
+import ir.hosseindn.model.Order;
 import ir.hosseindn.model.Technician;
 import ir.hosseindn.service.technician.TechnicianService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,19 +28,17 @@ import java.io.IOException;
 public class TechnicianController {
     private final TechnicianService technicianService;
 
-    @PostMapping("/technician-register")
-    public ResponseEntity<TechnicianSaveResponse> technicianRegister(@Validated @RequestBody TechnicianSaveRequest request) throws IOException {
-        Technician mappedTechnician = TechnicianMapper.INSTANCE.technicianSaveRequestWithoutPathToModel(request.technician());
-        Technician savedTechnician = technicianService.register(mappedTechnician, request.imagePath());
+    @PostMapping(value = "/technician-register" ,consumes = {MediaType.MULTIPART_FORM_DATA_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<TechnicianSaveResponse> technicianRegister(@Validated TechnicianSaveRequest request) throws IOException {
+        Technician mappedTechnician = TechnicianMapper.INSTANCE.technicianSaveRequestWithoutPathToModel(request);
+        Technician savedTechnician = technicianService.register(mappedTechnician, request.imageFile());
         return new ResponseEntity<>(TechnicianMapper.INSTANCE.modelToUserSaveResponse(savedTechnician), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasRole('ROLE_TECHNICIAN')")
     @PatchMapping("/technician-changePassword")
     public ResponseEntity<TechnicianChangePasswordResponse> technicianChangePassword(@Valid @RequestBody TechnicianChangePasswordRequest request) {
-        if (!request.password().equals(request.confirmPassword()))
-            throw new NotValidInformation("new password must be match by confirm");
-        Technician mappedTechnician = TechnicianMapper.INSTANCE.INSTANCE.technicianChangePasswordRequestToModel(request);
-        Technician savedTechnician = technicianService.changePassword(mappedTechnician.getEmail(), mappedTechnician.getPassword());
+        Technician savedTechnician = technicianService.checkConfirmationAndChangePassword(request);
         return new ResponseEntity<>(TechnicianMapper.INSTANCE.modelToTechnicianChangePasswordResponse(savedTechnician), HttpStatus.OK);
     }
 
@@ -45,11 +49,32 @@ public class TechnicianController {
         return new ResponseEntity<>(TechnicianMapper.INSTANCE.modelToTechnicianLoginResponse(LoggedInTechnician), HttpStatus.FOUND);
     }
 
+    @PreAuthorize("hasRole('ROLE_TECHNICIAN')")
     @PatchMapping("/technician-verify")
     public ResponseEntity<TechnicianVerifyResponse> technicianVerify(@Valid @RequestBody TechnicianVerifyRequest request) {
         Technician mappedTechnician = TechnicianMapper.INSTANCE.technicianVerifyRequestToModel(request);
         Technician changedStatusToVerify = technicianService.changeStatusToVerify(mappedTechnician);
         return new ResponseEntity<>(TechnicianMapper.INSTANCE.INSTANCE.modelToTechnicianVerifyResponse(changedStatusToVerify), HttpStatus.OK);
+    }
+
+    @PreAuthorize("hasRole('ROLE_TECHNICIAN')")
+    @GetMapping("/technician-order-history")
+    public ResponseEntity<List<SeeTechnicianOrdersResponse>> technicianOrderHistory(@Valid @RequestParam String orderStatus,
+                                                                                  Principal principal) {
+        List<Order> orderList = technicianService.ordersHistory(principal.getName(), orderStatus);
+        return new ResponseEntity<>(OrderMapper.INSTANCE.modelListToSeeTechnicianOrdersResponse(orderList), HttpStatus.FOUND);
+    }
+
+    @PreAuthorize("hasRole('ROLE_TECHNICIAN')")
+    @GetMapping("/technician-wallet")
+    public ResponseEntity<Long> technicianSeeWallet(Principal principal) {
+        return new ResponseEntity<>(technicianService.getWalletAmount(principal.getName()), HttpStatus.FOUND);
+    }
+
+    @GetMapping("/see-rate")
+    @PreAuthorize("hasRole('ROLE_TECHNICIAN')")
+    public ResponseEntity<Double> getRate(Principal principal){
+        return new ResponseEntity<>(technicianService.getTechnicianRate(principal.getName()),HttpStatus.FOUND);
     }
 
 }
